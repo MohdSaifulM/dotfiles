0c71273e948ef7c76a60c46bcb5b8f68b2a56907
SaifM <mohd.saiful@live.com>
2026-07-24 17:53:46 +0800

    update TS config

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,27 @@
 return {
     {
         'nvim-treesitter/nvim-treesitter',
+        branch = 'main',
+        lazy = false,
         build = ':TSUpdate',
         config = function()
-            require("nvim-treesitter.configs").setup({
-                auto_install = true,
-                sync_install = false,
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
-                indent = { enable = true },
-                autotag = {
-                    enable = true,
-                }
+            require("nvim-treesitter").install({
+                "lua", "vim", "vimdoc", "query",
+                "markdown", "markdown_inline",
+                "javascript", "typescript", "tsx",
+                "html", "css", "json", "bash",
+            })
+
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function(args)
+                    local lang = vim.treesitter.language.get_lang(args.match) or args.match
+                    local ok = pcall(vim.treesitter.start)
+                    if ok then
+                        vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                    elseif vim.tbl_contains(require("nvim-treesitter").get_available(1), lang) then
+                        require("nvim-treesitter").install({ lang })
+                    end
+                end,
             })
         end
     }
